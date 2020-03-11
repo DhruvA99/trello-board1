938bf0113c5d0d73f1c9ff39407d2f6457efe27f
DhruvArora <dhruvarora9931@gmail.com>
2020-03-11 15:35:54 +0530

    Updates

---
+++ precache-manifest.7d773834d4930db1494597e9cd8907a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8bd1cda62a5f3e62d41cd475d8cc742",
+    "url": "/trello-board1/index.html"
+  },
+  {
+    "revision": "64a54600753b9a502b4e",
+    "url": "/trello-board1/static/css/main.598d22a2.chunk.css"
+  },
+  {
+    "revision": "fa68e2aa55a3c15034be",
+    "url": "/trello-board1/static/js/2.715aa770.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/trello-board1/static/js/2.715aa770.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64a54600753b9a502b4e",
+    "url": "/trello-board1/static/js/main.4113418e.chunk.js"
+  },
+  {
+    "revision": "2429a7950670e93be2ca",
+    "url": "/trello-board1/static/js/runtime-main.7a2fe197.js"
+  }
+]);
